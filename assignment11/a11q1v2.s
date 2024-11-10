0918159558796a2b9f57b61a0b928ec70fb1708d
yuefu-jiang <albertajiang@gmail.com>
2024-11-10 03:21:32 +0000

    added comments before submission

--- assignment11/a11q1v2.s
+++ assignment11/a11q1v2.s
@@ -1,23 +1,26 @@
 .text
 .global main
 
+# implemented m * n by recursion
+# resursive method: Mult
 main:
+	# for main, only needs #4 in sp
 	SUB sp, sp, #4
 	STR lr, [sp, #0]
 	
+	# load prompt for input number m
 	LDR r0, =promptm
 	BL printf
-	
+	# load num m in r6 for now
 	LDR r0, =format
 	LDR r1, =num_m
 	BL scanf
-
 	LDR r6, =num_m
 	LDR r6, [r6]
-
+	# load prompt for n
 	LDR r0, =promptn
 	BL printf
-
+	# store n in r7 for now
 	LDR r0, =format
 	LDR r1, =num_n
 	BL scanf
@@ -25,16 +28,14 @@ main:
 	LDR r7, =num_n
 	LDR r7, [r7]
 
-
 	# recursive mult
-	# r0: m
-	# r1, n
-	# MOV r0, #10
-	# MOV r1, #5
-
+	# r0: m; move from r6
+	# r1, n; move from r7
 	MOV r0, r6
 	MOV r1, r7
+	# call Mult and return result in r0
 	BL Mult
+	# move result to r1 and print
 	MOV r1, r0
 	LDR r0, =output
 	BL printf
@@ -53,6 +54,7 @@ main:
 # end main
 .text
 Mult:
+	# mult method uses r4 and r5 within, so we store r4 and r5 in stack first
 	SUB sp, sp, #16
 	STR lr, [sp, #0]
 	STR r4, [sp, #4]
@@ -66,9 +68,9 @@ Mult:
 	BEQ return0 //return 0 in r0
 
 	ADD r1, r5, #-1 //(n-1)
-	BL Mult //return value in r0
+	BL Mult //call recursion Mult(m, n-1)
 	ADD r0, r4, r0 //return mult in r0
-	B return //not really needed
+	B return 
 
 
 	return0:
@@ -81,4 +83,4 @@ Mult:
 		MOV pc, lr
 
 .data
-# end sum
+# end Mult
--- assignment11/a11q2.s
+++ assignment11/a11q2.s
@@ -6,17 +6,19 @@ main:
 	STR lr, [sp, #0]
 
 	# recursive fibonacci
+	# laod prompt for a integer
 	LDR r0, =prompt
 	BL printf
-
+	# scanf into r0
 	LDR r0, =format
 	LDR r1, =num
 	BL scanf
-
 	LDR r0, =num
 	LDR r0, [r0]
-
+	
+	# call recursive function, returns result in r0
 	BL Fibonacci
+	# move into r1 and printf result
 	MOV r1, r0
 	LDR r0, =output
 	BL printf
@@ -33,28 +35,31 @@ main:
 # end main
 .text
 Fibonacci:
+	# in Fibonacci, we uses r4 and r5, so stroring them in stack first
 	SUB sp, sp, #16
 	STR lr, [sp, #0]
 	STR r4, [sp, #4]
 	STR r5, [sp, #8]
+	# move r0 in to r4. This is the n value in F(n)
 	MOV r4, r0
 
-	# if r0 is 0, return 0
+	# if n is 0, return 0
 	MOV r1, #1
 	CMP r1, r4
 	BEQ return //return 0 in r0
 
-	# if r0 is 1, return 1
+	# if n is 1, return 1
 	MOV r1, #0
 	CMP r1, r4
 	BEQ return //return 0 in r0
-
+	
+	# now we subtract r0 by 1
 	ADD r0, r4, #-1
-	BL Fibonacci //return value in r0
-	MOV r5, r0 //store F(n-1) in r5
-	ADD r0, r4, #-2
-	BL Fibonacci //return value in r0
-	ADD r0, r5, r0 //return summation in r0
+	BL Fibonacci //call F(n-1)
+	MOV r5, r0 // store F(n-1) in r5
+	ADD r0, r4, #-2 // get n-2
+	BL Fibonacci // call F(n-2), stored in r0
+	ADD r0, r5, r0 // add f(n-2) and F(n-2)
 	B return //not really needed
 
 
@@ -66,4 +71,4 @@ Fibonacci:
 		MOV pc, lr
 
 .data
-# end sum
+# end Fibonacci
